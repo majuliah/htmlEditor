b289ede1990a10be3f3e1137a9cc70b08880d6b5
Majulia <majulialcarvalho@gmail.com>
2022-03-15 22:13:13 -0300

    starting editor

---
+++ Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HTMLeditor
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            
+        }
+    }
+}
