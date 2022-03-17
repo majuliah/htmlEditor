208fb16a1d6838006f45b029d9a5d1c5eed647a8
Majulia <majulialcarvalho@gmail.com>
2022-03-17 14:01:43 -0300

    ending menu options

--- Menu.cs
+++ Menu.cs
@@ -14,7 +14,7 @@ namespace HTMLeditor
             WriteOption();
 
             short option = short.Parse(Console.ReadLine());
-
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -62,6 +62,25 @@ namespace HTMLeditor
             Console.WriteLine($"Option: ");
             
             
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine($"Editor!");
+                    break;
+                case 2: Console.WriteLine($"View!");
+                    break;
+                case 0:
+                {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show();
+                    break;
+            }
         }
     }
 }
